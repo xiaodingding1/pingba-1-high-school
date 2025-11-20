06e777960e779c38bfef7ff2095179c5dce06cc8
xiaodingding1 <1609008959@qq.com>
2025-11-20 18:57:25 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+// 引入 LeanCloud SDK
+// 注意：在 index.html 里已经通过 <script src="https://cdn1.lncld.net/static/js/3.14.1/av-min.js"></script> 引入了 SDK
+
+// 初始化 LeanCloud
+AV.init({
+  appId: 'C3dq2PBdG2bI1cD6WMtlXU6y-gzGzoHsz',
+  appKey: 'gQCrkPGwovInr3mVcBVoF10s',
+  serverURL: 'https://C3dq2PBdG2bI1cD6WMtlXU6y.lc-cn-n1-shared.com'
+});
